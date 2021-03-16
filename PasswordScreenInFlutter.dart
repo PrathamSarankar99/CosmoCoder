6e5520a51851f37e8f660d3cb7981272e832057d
PrathamSarankar99 <75827914+PrathamSarankar99@users.noreply.github.com>
2021-03-16 09:00:12 +0530

    Create PasswordScreenInFlutter.dart

---
+++ PasswordScreenInFlutter.dart
@@ -0,0 +1,341 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:zopek/Services/Constants.dart';
+
+class PasswordView extends StatefulWidget {
+  @override
+  _PasswordViewState createState() => _PasswordViewState();
+}
+
+class _PasswordViewState extends State<PasswordView> {
+  var selectedindex = 0;
+  String code = '';
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(
+      fontSize: 25,
+      fontWeight: FontWeight.w500,
+      color: Colors.black.withBlue(40),
+    );
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    print("Code is $code");
+    return Scaffold(
+      backgroundColor: Colors.black.withBlue(40),
+      body: Column(
+        children: [
+          Container(
+            height: height * 0.15,
+            width: width,
+            color: Colors.black.withBlue(40),
+            alignment: Alignment.center,
+            child: SafeArea(
+              child: Container(
+                  height: height * 0.06,
+                  width: height * 0.06,
+                  child: SvgPicture.asset(
+                    'assets/incognito.svg',
+                  )),
+            ),
+          ),
+          Container(
+              height: height * 0.85,
+              width: width,
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade100,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: Text(
+                              "Daniel Johnson",
+                              style: TextStyle(
+                                fontSize: 23,
+                                color: Colors.black.withBlue(100),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Text(
+                              "daniel123@gmail.com",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black.withBlue(40),
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            DigitHolder(
+                              width: width,
+                              index: 0,
+                              selectedIndex: selectedindex,
+                              code: code,
+                            ),
+                            DigitHolder(
+                                width: width,
+                                index: 1,
+                                selectedIndex: selectedindex,
+                                code: code),
+                            DigitHolder(
+                                width: width,
+                                index: 2,
+                                selectedIndex: selectedindex,
+                                code: code),
+                            DigitHolder(
+                                width: width,
+                                index: 3,
+                                selectedIndex: selectedindex,
+                                code: code),
+                          ],
+                        )),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(1);
+                                          },
+                                          child: Text('1', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(2);
+                                          },
+                                          child: Text('2', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(3);
+                                          },
+                                          child: Text('3', style: textStyle)),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(4);
+                                          },
+                                          child: Text('4', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(5);
+                                          },
+                                          child: Text('5', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(6);
+                                          },
+                                          child: Text('6', style: textStyle)),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(7);
+                                          },
+                                          child: Text('7', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(8);
+                                          },
+                                          child: Text('8', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(9);
+                                          },
+                                          child: Text('9', style: textStyle)),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            backspace();
+                                          },
+                                          child: Icon(Icons.backspace_outlined,
+                                              color: Colors.black.withBlue(40),
+                                              size: 30)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {
+                                            addDigit(0);
+                                          },
+                                          child: Text('0', style: textStyle)),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: FlatButton(
+                                          height: double.maxFinite,
+                                          onPressed: () {},
+                                          child: Icon(Icons.check,
+                                              color: Colors.black.withBlue(40),
+                                              size: 30)),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+
+  addDigit(int digit) {
+    if (code.length > 3) {
+      return;
+    }
+    setState(() {
+      code = code + digit.toString();
+      print('Code is $code');
+      selectedindex = code.length;
+    });
+  }
+
+  backspace() {
+    if (code.length == 0) {
+      return;
+    }
+    setState(() {
+      code = code.substring(0, code.length - 1);
+      selectedindex = code.length;
+    });
+  }
+}
+
+class DigitHolder extends StatelessWidget {
+  final int selectedIndex;
+  final int index;
+  final String code;
+  const DigitHolder({
+    @required this.selectedIndex,
+    Key key,
+    @required this.width,
+    this.index,
+    this.code,
+  }) : super(key: key);
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: width * 0.17,
+      width: width * 0.17,
+      margin: EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: index == selectedIndex ? Colors.blue : Colors.transparent,
+              offset: Offset(0, 0),
+              spreadRadius: 1.5,
+              blurRadius: 2,
+            )
+          ]),
+      child: code.length > index
+          ? Container(
+              width: 15,
+              height: 15,
+              decoration: BoxDecoration(
+                color: Colors.black.withBlue(40),
+                shape: BoxShape.circle,
+              ),
+            )
+          : Container(),
+    );
+  }
+}
